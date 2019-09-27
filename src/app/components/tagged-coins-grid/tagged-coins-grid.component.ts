36968ad3c4612ad254fcd5beccd1a167234f9e10
VBogomolov <vbogomolov@infragistics.com>
2019-09-27 22:12:27 +0300

    feat(): added volume stream and changed the way the grid shows entries

--- src/app/components/tagged-coins-grid/tagged-coins-grid.component.ts
+++ src/app/components/tagged-coins-grid/tagged-coins-grid.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { StoreService } from 'src/app/services/store/store.service';
-import { ObjectKlines, TaggedGridEntry } from 'src/app/interfaces';
-import { LIMIT } from 'src/app/consts';
-import { calculateDifferenceForInterval } from 'src/app/helpers';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { StoreService } from "src/app/services/store/store.service";
+import { ObjectKlines, TaggedGridEntry } from "src/app/interfaces";
+import { LIMIT } from "src/app/consts";
+import { calculateDifferenceForInterval } from "src/app/helpers";
+import { TimeInterval } from "rxjs";
 
 @Component({
-  selector: 'app-tagged-coins-grid',
-  templateUrl: './tagged-coins-grid.component.html',
-  styleUrls: ['./tagged-coins-grid.component.scss']
+  selector: "app-tagged-coins-grid",
+  templateUrl: "./tagged-coins-grid.component.html",
+  styleUrls: ["./tagged-coins-grid.component.scss"]
 })
-export class TaggedCoinsGridComponent implements OnInit {
-  public data = [];
+export class TaggedCoinsGridComponent implements OnInit, OnDestroy {
+  private interval;
+  private timer = 0;
+  public loading = true;
+  public data: TaggedGridEntry[] = [];
 
   constructor(private storeService: StoreService) {}
 
@@ -21,27 +25,60 @@ export class TaggedCoinsGridComponent implements OnInit {
     });
   }
 
-  private convertToGridData(coinKlinesData: ObjectKlines) {
-    const data = [];
-    for (const key in coinKlinesData) {
-      if (coinKlinesData[key].tagged) {
-        const currentCoin = coinKlinesData[key];
-        const entry: TaggedGridEntry = {
-          Ticker: currentCoin.coin.symbol,
-          Price: Number(currentCoin.data[LIMIT - 1].ClosePrice),
-          '% change(3m)': calculateDifferenceForInterval(currentCoin.data, 3),
-          '% change(15m)': calculateDifferenceForInterval(currentCoin.data, 15),
-          '% change(1h)': calculateDifferenceForInterval(currentCoin.data, 60),
-          '% change(4h)': calculateDifferenceForInterval(currentCoin.data, 60 * 4)
-        };
-        data.push(entry);
-      }
-    }
-    return data;
-
+  ngOnDestroy() {
+    this.interval.clearInterval();
   }
 
   public formatValue(value: number) {
     return value.toFixed(8);
+  }
+
+  private convertToGridData(coinKlinesData: ObjectKlines) {
+    const newData =[];
+    for (const key in coinKlinesData) {
+      const currentCoin = coinKlinesData[key];
+      if (coinKlinesData[key].tagged) {
+        const match = this.data.find(
+          (el: TaggedGridEntry) => el.Ticker === key
+        );
+        if (match) {
+            match.Price = Number(currentCoin.data[LIMIT - 1].ClosePrice);
+            match["% change(3m)"] = calculateDifferenceForInterval(currentCoin.data, 3);
+            match["% change(15m)"] = calculateDifferenceForInterval(currentCoin.data,15);
+            match["% change(1h)"] = calculateDifferenceForInterval(currentCoin.data,60);
+            match["% change(4h)"] = calculateDifferenceForInterval(currentCoin.data, 60 * 4);
+            newData.push(match)
+        } else {
+          const entry: TaggedGridEntry = {
+            Ticker: currentCoin.coin.symbol,
+            Price: Number(currentCoin.data[LIMIT - 1].ClosePrice),
+            "% change(3m)": calculateDifferenceForInterval(currentCoin.data, 3),
+            "% change(15m)": calculateDifferenceForInterval(currentCoin.data,15),
+            "% change(1h)": calculateDifferenceForInterval(currentCoin.data,60),
+            "% change(4h)": calculateDifferenceForInterval(currentCoin.data,60 * 4),
+            date: new Date()
+          };
+          newData.push(entry)
+        }
+      }
+    }
+    if (this.loading) {
+      this.timer = 60 - new Date().getSeconds();
+      this.loading = false;
+      this.interval = setInterval(() => {
+        this.timer--;
+        if (this.timer === 0) {
+          this.timer = 65;
+        }
+      }, 1000);
+    }
+    return this.sortByDate(newData);
+  }
+
+  private sortByDate(arr: TaggedGridEntry[]) {
+    return arr.sort(
+      (a: TaggedGridEntry, b: TaggedGridEntry) =>
+        b.date.getTime() - a.date.getTime()
+    );
   }
 }
